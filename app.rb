1f7379ef00897c551b8ef45928e84fd443101c89
Kevin Lanzon <kevinlanzon@gmail.com>
2016-05-02 15:31:08 +0100

    running on heroku

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ get '/' do
   erb :index
 end
 
+get '/domestic_portraits' do
+  erb :domestic_portraits
+end
+
+get '/large_paintings' do
+  erb :large_paintings
+end
+
+get '/recent_exhibitions' do
+  erb :recent_exhibitions
+end
+
 get '/about' do
   erb :about
+end
+
+get '/contact' do
+  erb :contact
 end
